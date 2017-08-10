04641b8e92acf839abb6d32e04c6e46fdbdd0a9a
Kyle Birch <mysecureview@kylebirch.info>
2017-08-10 13:03:14 -0600

    Banner design & test cases
    run unit_test.php, which executes the code that is uncommented, and
    displays the result in blog.php.
    
    This requires php 7. If you want it to work with earlier versions, you
    need to change the method parameters.

---
+++ unit_test.php
@@ -0,0 +1,14 @@
+<?php
+//comment out code to see what the rest does
+
+$note = new Notification();
+
+$note->addNote('success', 'Congrats! You are now logged in.');
+
+//$note->addNote('warning', 'Your password has expired. You need to reset it.');
+
+//$note->addNote('error', 'cannot connect to the database.');
+
+//$note->addNote('info','The website will be down for maintenence Saturday, August 5th from 12:00am to 2am, MDT. We are sorry for the inconvenience.');
+
+//$note->addMultipleNotes('success', 'You are now logged in', 'warning', 'Your password expired. It must be reset.');
