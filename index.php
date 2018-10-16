b97078f8b1995ff7b8b5238ec6106c879fe7ed53
Kyle Birch <theflyinginvalid@gmail.com>
2018-10-15 19:44:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,9 @@
 		<?php 
 		$note = new Notification();
 		$note->addNote('success', 'Congrats! You are now logged in.');
-		//$note->addNote('warning', 'Your password has expired. You need to reset it.');
+		$note->addNote('warning', 'Your password has expired. You need to reset it.');
 		//$note->addNote('error', 'cannot connect to the database.');
 		//$note->addNote('info','The website will be down for maintenence Saturday, August 5th from 12:00am to 2am, MDT. We are sorry for the inconvenience.');
-		//$note->addMultipleNotes('success', 'You are now logged in', 'warning', 'Your password expired. It must be reset.');
 		
 		$note->notify(); 
 		
